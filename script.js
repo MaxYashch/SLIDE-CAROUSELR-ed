39290d68218d9b07f758d0fb728ab9bab5ebf237
MaxYashch <maxyashch@gmail.com>
2020-10-20 09:55:31 +0300

    strt carousel

---
+++ script.js
@@ -0,0 +1,9 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+// buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+// counter
+let counter = 1;
